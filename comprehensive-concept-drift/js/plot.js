8bc5692e5a72985adb51f8c76ef7cc260379f273
gabrieljaguiar <gja.gbrl@gmail.com>
2023-11-10 12:57:31 -0500

    update links

--- comprehensive-concept-drift/js/plot.js
+++ comprehensive-concept-drift/js/plot.js
@@ -352,7 +352,8 @@ function getData(experiment, generatorIdx, difficultIdx, number_of_classes, numb
 
   //HT_ADWIN_no_drift_hyp_f_2_c_10_1: 1
   // HT_ADWIN_intra_class_drift_emerging_branch_rt_ds_1_f_2_c_2_1: 1
-  return ["./output/" + csv_file, "./datasets/" + data_file];
+  html_prefix = "https://www.cmsc508.com/~aguiargj/"
+  return [http_prefix+"output/" + csv_file, http_prefix+"/datasets/" + data_file];
 }
 
 
